bfab1db2e8616b87bb9f6d9840ffe76b72743816
Youssef <youssef.dev.hq@gmail.com>
2025-12-30 20:42:56 +0200

    link all modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NotFoundFilter } from './common/filters/not-found.filter';
+import { ErrorFilter } from './common/filters/errors.filter';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new ErrorFilter(), new NotFoundFilter());
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Application running on http://localhost:${port}`);
 }
 bootstrap();
