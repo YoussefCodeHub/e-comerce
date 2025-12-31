d05645f543658e192f25a3275309ebd71a792090
Youssef <youssef.dev.hq@gmail.com>
2025-12-31 11:14:07 +0200

    add brand module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { BrandModule } from './modules/brand/brand.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { DatabaseModule } from './database/database.module';
     DatabaseModule,
     AuthModule,
     UserModule,
+    BrandModule,
   ],
 })
 export class AppModule {}
