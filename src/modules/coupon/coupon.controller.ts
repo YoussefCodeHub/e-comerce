332596261ddee98ab27687f01a152a5d01625bdf
Youssef <youssef.dev.hq@gmail.com>
2025-12-31 22:08:04 +0200

    fix coupon module to apply role access

--- src/modules/coupon/coupon.controller.ts
+++ src/modules/coupon/coupon.controller.ts
@@ -2,13 +2,16 @@ import { Controller, Post, Body, Req, UseGuards } from '@nestjs/common';
 import { CouponService } from './coupon.service';
 import { AuthGuard } from '../../common/guards/auth.guard';
 import { CreateCouponDto, ApplyCouponDto } from './coupon.dto';
+import { RolesGuard } from '../../common/guards/roles.guard';
+import { Roles } from '../../common/decorators/roles.decorator';
 
 @Controller('coupon')
-@UseGuards(AuthGuard)
 export class CouponController {
   constructor(private readonly couponService: CouponService) {}
 
   @Post('create')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles('admin')
   async createCoupon(@Body() data: CreateCouponDto, @Req() req: any) {
     const coupon = await this.couponService.create(
       data.code,
@@ -21,6 +24,7 @@ export class CouponController {
   }
 
   @Post('apply')
+  @UseGuards(AuthGuard)
   async applyCoupon(@Body() data: ApplyCouponDto, @Req() req: any) {
     const cart = await this.couponService.applyCoupon(req.user._id, data.code);
     return { status: 'success', message: 'Coupon applied', data: cart };
